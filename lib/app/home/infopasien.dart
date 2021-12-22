5b2d781973d89d0e859c68cde06db2954d02e683
SilentRoar <silentroar77@gmail.com>
2021-12-23 01:56:27 +0700

    22/12

--- lib/app/home/infopasien.dart
+++ lib/app/home/infopasien.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:line_icons/line_icons.dart';
+
 class infopasien extends StatefulWidget {
   const infopasien({Key? key}) : super(key: key);
 
@@ -23,7 +24,6 @@ class _infopasienState extends State<infopasien> {
     return Container(
       decoration: BoxDecoration(
         color: Colors.blue,
-
       ),
       width: MediaQuery.of(context).size.width,
       height: 80,
@@ -34,13 +34,16 @@ class _infopasienState extends State<infopasien> {
           children: [
             Column(
               children: [
-                Text('hello', style: TextStyle(color: Colors.white),),
+                Text(
+                  'hello',
+                  style: TextStyle(color: Colors.white),
+                ),
                 StreamBuilder<DocumentSnapshot>(
                   stream: userdata.doc(emaila.toString()).snapshots(),
                   builder: (context, AsyncSnapshot snapshot) {
                     if (snapshot.hasData) {
                       Map<String, dynamic> data =
-                      snapshot.data!.data() as Map<String, dynamic>;
+                          snapshot.data!.data() as Map<String, dynamic>;
 
                       String nama;
                       nama = data['nama'];
@@ -50,7 +53,7 @@ class _infopasienState extends State<infopasien> {
                         style: GoogleFonts.pathwayGothicOne(
                             fontWeight: FontWeight.w500,
                             fontSize: 20,
-                        color: Colors.white),
+                            color: Colors.white),
                       );
                     }
                     return Center(
@@ -61,7 +64,8 @@ class _infopasienState extends State<infopasien> {
               ],
             ),
             Icon(
-                LineIcons.info,
+              LineIcons.cog,
+              size: 32,
               color: Colors.white,
             )
           ],
--- lib/app/home/tombol2.dart
+++ lib/app/home/tombol2.dart
@@ -13,87 +13,132 @@ class _tomboltombolState extends State<tomboltombol> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 1,
-                      blurStyle: BlurStyle.outer,
-                    )
-                  ],
-                ),
-                width: MediaQuery.of(context).size.width / 5,
-                height: MediaQuery.of(context).size.width / 5,
-                child: Icon(
-                  LineIcons.doctor,
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text('riwayat'),
+      padding: EdgeInsets.only(left: 16, right: 16, top: 20, bottom: 20),
+      child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 1,
+                blurStyle: BlurStyle.outer,
+              )
             ],
           ),
-          Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 1,
-                      blurStyle: BlurStyle.outer,
-                    )
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width / 3,
+          child: Padding(
+            padding: EdgeInsets.only(left: 2, right: 2, top: 20, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                    child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 1,
+                            blurStyle: BlurStyle.outer,
+                          )
+                        ],
+                      ),
+                      width: MediaQuery.of(context).size.width / 7,
+                      height: MediaQuery.of(context).size.width / 7,
+                      child: Icon(
+                        LineIcons.stethoscope,size: 32,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('konsultasi'),
                   ],
-                ),
-                width: MediaQuery.of(context).size.width / 5,
-                height: MediaQuery.of(context).size.width / 5,
-                child: Icon(
-                  LineIcons.doctor,
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text('konsultasi'),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 1,
-                      blurStyle: BlurStyle.outer,
-                    )
+                )),
+                Expanded(
+                    child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 1,
+                            blurStyle: BlurStyle.outer,
+                          )
+                        ],
+                      ),
+                      width: MediaQuery.of(context).size.width / 7,
+                      height: MediaQuery.of(context).size.width / 7,
+                      child: Icon(
+                        LineIcons.medicalNotes,size: 32,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('riwayat'),
                   ],
-                ),
-                width: MediaQuery.of(context).size.width / 5,
-                height: MediaQuery.of(context).size.width / 5,
-                child: Icon(
-                  LineIcons.doctor,
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text('atur jadwal'),
-            ],
-          ),
-        ],
-      ),
+                )),
+                Expanded(
+                    child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 1,
+                            blurStyle: BlurStyle.outer,
+                          )
+                        ],
+                      ),
+                      width: MediaQuery.of(context).size.width / 7,
+                      height: MediaQuery.of(context).size.width / 7,
+                      child: Icon(
+                        LineIcons.calendarAlt,size: 32,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('Atur Jadwal'),
+                  ],
+                )),
+                Expanded(
+                    child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 1,
+                            blurStyle: BlurStyle.outer,
+                          )
+                        ],
+                      ),
+                      width: MediaQuery.of(context).size.width / 7,
+                      height: MediaQuery.of(context).size.width / 7,
+                      child: Icon(
+                        LineIcons.horizontalEllipsis,size: 32,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('riwayat'),
+                  ],
+                )),
+              ],
+            ),
+          )),
     );
   }
 }
